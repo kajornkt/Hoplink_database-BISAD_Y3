aa178ee6fc3eda663900211659805942f3486dbc
Khokwan Tachaongart <khokwan16@gmail.com>
2025-09-17 00:40:59 +0700

    Update data-line.sql

--- data-line.sql
+++ data-line.sql
@@ -7,7 +7,7 @@ MAXVALUE 9999999;
 INSERT INTO Line VALUES (1, 4, 'สายสีเขียวอ่อน', 'Light green line', 'Yes');
 INSERT INTO Line VALUES (2, 3, 'สายสีฟ้า', 'Blue line', 'Yes');
 INSERT INTO Line VALUES (3, 4, 'สายสีเหลือง', 'Yellow line', 'Yes');
-INSERT INTO Line VALUES (4, 5, 'สายตะวันออก', 'East line', 'Yes');
+INSERT INTO Line VALUES (4, 5, 'สายตะวันออก', 'Eastern line', 'Yes');
 INSERT INTO Line VALUES (5, 6, 'ARL', 'ARL', 'Yes');
 INSERT INTO Line VALUES (6, 2, '2-38', '2-38', 'Yes');
 INSERT INTO Line VALUES (7, 1, '13 (3-38)', '13 (3-38)', 'Yes');
