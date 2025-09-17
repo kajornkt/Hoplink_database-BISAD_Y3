79eea65a77a7050b08bfac24d15cea81b26a137a
Khokwan Tachaongart <khokwan16@gmail.com>
2025-09-17 12:04:41 +0700

    Update vehicle.sql

--- vehicle.sql
+++ vehicle.sql
@@ -7,45 +7,45 @@ MAXVALUE 99999;
 -- ==========================
 -- BMTA Buses
 -- ==========================
-INSERT INTO Vehicle VALUES (vehicle_id_seq, 1, 7);
-INSERT INTO Vehicle VALUES (vehicle_id_seq, 1, 7);
-INSERT INTO Vehicle VALUES (vehicle_id_seq, 1, 8);
-INSERT INTO Vehicle VALUES (vehicle_id_seq, 1, 8);
-INSERT INTO Vehicle VALUES (vehicle_id_seq, 1, 11);
-INSERT INTO Vehicle VALUES (vehicle_id_seq, 1, 11);
+INSERT INTO Vehicle VALUES (vehicle_id_seq, 1, 7); -- 1
+INSERT INTO Vehicle VALUES (vehicle_id_seq, 1, 7); -- 2
+INSERT INTO Vehicle VALUES (vehicle_id_seq, 1, 8); -- 3
+INSERT INTO Vehicle VALUES (vehicle_id_seq, 1, 8); -- 4
+INSERT INTO Vehicle VALUES (vehicle_id_seq, 1, 11); -- 5
+INSERT INTO Vehicle VALUES (vehicle_id_seq, 1, 11); -- 6
 
 -- ==========================
 -- TSB Buses
 -- ==========================
-INSERT INTO Vehicle VALUES (vehicle_id_seq, 2, 6);
-INSERT INTO Vehicle VALUES (vehicle_id_seq, 2, 6);
-INSERT INTO Vehicle VALUES (vehicle_id_seq, 2, 9);
-INSERT INTO Vehicle VALUES (vehicle_id_seq, 2, 9);
-INSERT INTO Vehicle VALUES (vehicle_id_seq, 2, 10);
-INSERT INTO Vehicle VALUES (vehicle_id_seq, 2, 10);
+INSERT INTO Vehicle VALUES (vehicle_id_seq, 2, 6); -- 7
+INSERT INTO Vehicle VALUES (vehicle_id_seq, 2, 6); -- 8
+INSERT INTO Vehicle VALUES (vehicle_id_seq, 2, 9); -- 9
+INSERT INTO Vehicle VALUES (vehicle_id_seq, 2, 9); -- 10
+INSERT INTO Vehicle VALUES (vehicle_id_seq, 2, 10); -- 11
+INSERT INTO Vehicle VALUES (vehicle_id_seq, 2, 10); -- 12
 
 -- ==========================
 -- MRT Trains
 -- ==========================
-INSERT INTO Vehicle VALUES (vehicle_id_seq, 3, 2);
-INSERT INTO Vehicle VALUES (vehicle_id_seq, 3, 2);
-INSERT INTO Vehicle VALUES (vehicle_id_seq, 3, 3);
-INSERT INTO Vehicle VALUES (vehicle_id_seq, 3, 3);
+INSERT INTO Vehicle VALUES (vehicle_id_seq, 3, 2); -- 13
+INSERT INTO Vehicle VALUES (vehicle_id_seq, 3, 2); -- 14
+INSERT INTO Vehicle VALUES (vehicle_id_seq, 3, 3); -- 15
+INSERT INTO Vehicle VALUES (vehicle_id_seq, 3, 3); -- 16
 
 -- ==========================
 -- BTS Trains
 -- ==========================
-INSERT INTO Vehicle VALUES (vehicle_id_seq, 4, 1);
-INSERT INTO Vehicle VALUES (vehicle_id_seq, 4, 1);
+INSERT INTO Vehicle VALUES (vehicle_id_seq, 4, 1); -- 17
+INSERT INTO Vehicle VALUES (vehicle_id_seq, 4, 1); -- 18
 
 -- ==========================
 -- SRT Trains
 -- ==========================
-INSERT INTO Vehicle VALUES (vehicle_id_seq, 5, 4);
-INSERT INTO Vehicle VALUES (vehicle_id_seq, 5, 4);
+INSERT INTO Vehicle VALUES (vehicle_id_seq, 5, 4); -- 19
+INSERT INTO Vehicle VALUES (vehicle_id_seq, 5, 4); -- 20
 
 -- ==========================
 -- ARL Trains
 -- ==========================
-INSERT INTO Vehicle VALUES (vehicle_id_seq, 6, 5);
-INSERT INTO Vehicle VALUES (vehicle_id_seq, 6, 5);
+INSERT INTO Vehicle VALUES (vehicle_id_seq, 6, 5); -- 21
+INSERT INTO Vehicle VALUES (vehicle_id_seq, 6, 5); -- 22
