5bd80c18b50b272915a19c99ef6411662765ef4d
Khokwan Tachaongart <khokwan16@gmail.com>
2025-09-17 11:40:25 +0700

    Create vehicle.sql

---
+++ vehicle.sql
@@ -0,0 +1,51 @@
+CREATE SEQUENCE vehicle_id_seq
+START WITH 00001
+INCREMENT BY 1
+MAXVALUE 99999;
+-- vehicle_id, operator_id, line_id
+
+-- ==========================
+-- BMTA Buses
+-- ==========================
+INSERT INTO Vehicle VALUES (vehicle_id_seq, 1, 7);
+INSERT INTO Vehicle VALUES (vehicle_id_seq, 1, 7);
+INSERT INTO Vehicle VALUES (vehicle_id_seq, 1, 8);
+INSERT INTO Vehicle VALUES (vehicle_id_seq, 1, 8);
+INSERT INTO Vehicle VALUES (vehicle_id_seq, 1, 11);
+INSERT INTO Vehicle VALUES (vehicle_id_seq, 1, 11);
+
+-- ==========================
+-- TSB Buses
+-- ==========================
+INSERT INTO Vehicle VALUES (vehicle_id_seq, 2, 6);
+INSERT INTO Vehicle VALUES (vehicle_id_seq, 2, 6);
+INSERT INTO Vehicle VALUES (vehicle_id_seq, 2, 9);
+INSERT INTO Vehicle VALUES (vehicle_id_seq, 2, 9);
+INSERT INTO Vehicle VALUES (vehicle_id_seq, 2, 10);
+INSERT INTO Vehicle VALUES (vehicle_id_seq, 2, 10);
+
+-- ==========================
+-- MRT Trains
+-- ==========================
+INSERT INTO Vehicle VALUES (vehicle_id_seq, 3, 2);
+INSERT INTO Vehicle VALUES (vehicle_id_seq, 3, 2);
+INSERT INTO Vehicle VALUES (vehicle_id_seq, 3, 3);
+INSERT INTO Vehicle VALUES (vehicle_id_seq, 3, 3);
+
+-- ==========================
+-- BTS Trains
+-- ==========================
+INSERT INTO Vehicle VALUES (vehicle_id_seq, 4, 1);
+INSERT INTO Vehicle VALUES (vehicle_id_seq, 4, 1);
+
+-- ==========================
+-- SRT Trains
+-- ==========================
+INSERT INTO Vehicle VALUES (vehicle_id_seq, 5, 4);
+INSERT INTO Vehicle VALUES (vehicle_id_seq, 5, 4);
+
+-- ==========================
+-- ARL Trains
+-- ==========================
+INSERT INTO Vehicle VALUES (vehicle_id_seq, 6, 5);
+INSERT INTO Vehicle VALUES (vehicle_id_seq, 6, 5);
