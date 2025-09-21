c8a60cc392311603b4bcf375ef0afbe12c03a214
Khokwan Tachaongart <khokwan16@gmail.com>
2025-09-22 00:40:35 +0700

    update

--- data-interchange.sql
+++ data-interchange.sql
@@ -9,10 +9,10 @@ MAXVALUE 99999;
 -- ==========================
 INSERT INTO Interchange VALUES (interchange_id_seq.nextval, 'พญาไท', 'Phayathai', 'Ratchathewi');
 INSERT INTO Interchange VALUES (interchange_id_seq.nextval, 'มักกะสัน/เพชรบุรี/อโศก', 'Makkasan/Phetchaburi/Asoke', 'Ratchathewi');
-INSERT INTO Interchange VALUES (interchange_id_seq.nextval, 'หัวหมาก', 'Hua Mak ', 'Suan Luang ');
-INSERT INTO Interchange VALUES (interchange_id_seq.nextval, 'หมอชิต/สวนจตุจักร', 'Mo Chit/Chatuchak Park ', 'Chatuchak');
+INSERT INTO Interchange VALUES (interchange_id_seq.nextval, 'หัวหมาก', 'Hua Mak', 'Suan Luang');
+INSERT INTO Interchange VALUES (interchange_id_seq.nextval, 'หมอชิต/สวนจตุจักร', 'Mo Chit/Chatuchak Park', 'Chatuchak');
 INSERT INTO Interchange VALUES (interchange_id_seq.nextval, 'ลาดพร้าว', 'Lat Phrao', 'Chatuchak');
-INSERT INTO Interchange VALUES (interchange_id_seq.nextval, 'พหลโยธิน/ห้าแยกลาดพร้าว ', 'Phahon Yothin/Ha Yaek Lat Phrao', 'Chatuchak');
+INSERT INTO Interchange VALUES (interchange_id_seq.nextval, 'พหลโยธิน/ห้าแยกลาดพร้าว', 'Phahon Yothin/Ha Yaek Lat Phrao', 'Chatuchak');
 
 -- ==========================
 -- Train-Bus Interchanges
--- data-ticket.sql
+++ data-ticket.sql
@@ -14,5 +14,5 @@ INSERT INTO Ticket VALUES (ticket_id_seq.nextval, 5, 'SRT14.2.4', 'BL13.2.9', TI
 INSERT INTO Ticket VALUES (ticket_id_seq.nextval, 6, 'SRT14.2.4', '2-38.1.54', TIMESTAMP '2025-09-04 07:10:24', 50, TIMESTAMP '2025-09-05 07:10:24', 'Used');
 INSERT INTO Ticket VALUES (ticket_id_seq.nextval, 6, 'SRT14.2.4', '2-38.1.54', TIMESTAMP '2025-09-20 07:10:24', 50, TIMESTAMP '2025-09-21 07:10:24', 'Active');
 
-INSERT INTO Ticket VALUES (ticket_id_seq.nextval, 6, 'SRT14.2.4', 'BL13.2.9', TIMESTAMP '2025-09-20 07:10:24', 29, TIMESTAMP '2025-09-21 07:10:24', 'Cancelled');
-INSERT INTO Ticket VALUES (ticket_id_seq.nextval, 6, 'A8.1.1', 'S5.1.22', TIMESTAMP '2025-09-20 07:24:24', 66, TIMESTAMP '2025-09-21 07:24:24', 'Active');
+INSERT INTO Ticket VALUES (ticket_id_seq.nextval, 7, 'SRT14.2.4', 'BL13.2.9', TIMESTAMP '2025-09-20 07:10:24', 29, TIMESTAMP '2025-09-21 07:10:24', 'Cancelled');
+INSERT INTO Ticket VALUES (ticket_id_seq.nextval, 7, 'A8.1.1', 'S5.1.22', TIMESTAMP '2025-09-20 07:24:24', 66, TIMESTAMP '2025-09-21 07:24:24', 'Active');
